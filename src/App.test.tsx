af72243584bb4b94c26ef6298b46c84503a6b887
jacekmalczyk <jacekmalczyk@jtendo.com>
2022-02-10 20:45:38 +0100

    refactor(App): Remove label function.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App, { label } from "./App";
+import App from "./App";
 
 test("renders default version", () => {
   render(<App />);
   const linkElement = screen.getByText(/tdd playground/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("renders custom label", () => {
-  render(<App />);
-  const result = label("TDD playground");
-  expect(result).toEqual("Hello TDD playground");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-export function label(name: string) {
-  return `Hello ${name}`;
-}
-
 function App() {
   return (
     <div>
-      <h1>{label("TDD playground")}</h1>
+      <h1>{"TDD playground"}</h1>
     </div>
   );
 }
